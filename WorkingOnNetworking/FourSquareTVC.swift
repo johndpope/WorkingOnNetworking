ca8b384e554ce5e033e70387a04f6725b356b722
Donovan Cotter <donovan.cotter@yahoo.com>
2016-01-20 11:08:36 -0500

    added locationManager to FourSquareTVC

--- WorkingOnNetworking/FourSquareTVC.swift
+++ WorkingOnNetworking/FourSquareTVC.swift
@@ -9,9 +9,12 @@
 import UIKit
 import MapKit
 
-class FourSquareTVC: UIViewController, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate {
+class FourSquareTVC: UIViewController, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager?
+    let distanceSpan: Double = 500
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +28,34 @@ class FourSquareTVC: UIViewController, UITableViewDelegate, UITableViewDataSourc
             mapView.delegate = self
         }
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        if locationManager == nil {
+            locationManager = CLLocationManager()
+            
+            locationManager!.delegate = self
+            locationManager!.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+            //This method will cause a popup in the app asking for permission to use the GPS location data.
+            locationManager!.requestAlwaysAuthorization()
+            locationManager!.distanceFilter = 50
+            //This will cause the location manager to poll for a GPS location, and call a method on the delegate telling it the new GPS location.
+            locationManager!.startUpdatingLocation()
+        
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //the method has 3 parameters: the location manager that called the method, the new GPS location, and the old GPS location.
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        if let mapView = self.mapView {
+            //a region is calculated from the new GPS coordinate and the distanceSpan (500 x 500) we created earlier with the newLocation coordinate in the center.
+            let region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, distanceSpan, distanceSpan)
+            mapView.setRegion(region, animated: true)
+        }
     }
     
     
